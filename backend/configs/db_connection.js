955ad14c023df6d5aa8a40680534bc7a305f3e0c
BelzDaniel <BelzDaniel6@gmail.com>
2024-10-25 02:27:36 +0300

    feat: adding logEvents function to the connectDB

--- backend/configs/db_connection.js
+++ backend/configs/db_connection.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
+const { logEvents } = require("../src/middleware/logger.js");
 
 /**
- * this async function initializing the connection to the mongo db.
+ * this asynchronous function initializing the connection to the MongoDB.
  */
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
-    console.log(error);
+    logEvents(
+      `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+      "mongo_error.log"
+    );
   }
 };
 
